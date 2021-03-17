f62f3ecf165a281f8cac01039964acb1a116e3db
Tomáš Stejskal <Stejsky211@seznam.cz>
2021-03-17 21:09:48 +0100

    Přidejte soubory projektu.

---
+++ Basic_Game/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Basic_Game
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random r = new Random();
+
+            int winNumber = r.Next(0, 100);
+
+            bool win = false;
+
+            int count = 0;
+
+            do
+            {
+                Console.WriteLine("Write a number between 0 and 100");
+
+                int number = int.Parse(Console.ReadLine());
+
+                if (number > winNumber)
+                {
+                    Console.WriteLine("To high!");
+                    count++;
+                }
+                else if (number < winNumber)
+                {
+                    Console.WriteLine("To low!");
+                    count++;
+                }
+                else if (number == winNumber)
+                {
+                    count++;
+                    Console.WriteLine("Bingo, YOU WIN!");
+                    Console.WriteLine("You needed " + count + " attempts");
+                    win = true;
+                }
+
+                Console.WriteLine(); ;
+            }
+            while (win == false);
+
+
+        }
+    }
+}
